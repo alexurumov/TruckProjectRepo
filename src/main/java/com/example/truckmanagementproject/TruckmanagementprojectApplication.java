1b3515a08048256bd8b0883a020f90e760a63cc6
alexurumov <alex.urumov@gmail.com>
2019-11-11 15:14:00 +0200

    Spring Sceleton Commit

---
+++ src/main/java/com/example/truckmanagementproject/TruckmanagementprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.truckmanagementproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckmanagementprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruckmanagementprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/truckmanagementproject/TruckmanagementprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.truckmanagementproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckmanagementprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
