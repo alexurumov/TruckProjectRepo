50310873e8bd49e452f907245000001a69cb5345
alexurumov <alex.urumov@gmail.com>
2019-11-16 17:27:01 +0200

    Upload picture to local dir Load pictures from local dir

--- src/main/java/truckmanagementproject/TruckmanagementprojectApplication.java
+++ src/main/java/truckmanagementproject/TruckmanagementprojectApplication.java
@@ -2,11 +2,15 @@ package truckmanagementproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import truckmanagementproject.web.controllers.UploadController;
+
+import java.io.File;
 
 @SpringBootApplication
 public class TruckmanagementprojectApplication {
 
     public static void main(String[] args) {
+        new File(UploadController.uploadDirectory).mkdir();
         SpringApplication.run(TruckmanagementprojectApplication.class, args);
 
     }
