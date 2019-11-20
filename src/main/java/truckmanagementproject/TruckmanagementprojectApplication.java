8dfdad8e03e94b8be0ae0e4b2cc8451f83c50d51
alexurumov <alex.urumov@gmail.com>
2019-11-20 21:34:51 +0200

    Remove Picture Upload Logic

--- src/main/java/truckmanagementproject/TruckmanagementprojectApplication.java
+++ src/main/java/truckmanagementproject/TruckmanagementprojectApplication.java
@@ -2,17 +2,14 @@ package truckmanagementproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import truckmanagementproject.web.controllers.UploadController;
-
 import java.io.File;
 
 @SpringBootApplication
 public class TruckmanagementprojectApplication {
 
     public static void main(String[] args) {
-        new File(UploadController.uploadDirectory).mkdir();
+//        new File(UploadController.uploadDirectory).mkdir();
         SpringApplication.run(TruckmanagementprojectApplication.class, args);
-
     }
 
 }
