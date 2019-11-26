710ade4d0b0e95a958d29003dec7980f09e613f7
alexurumov <alex.urumov@gmail.com>
2019-11-26 14:20:09 +0200

    Configured Cloudinary API and created Cloudinary Service for uploading a photo

---
+++ src/main/java/truckmanagementproject/config/CloudinaryConfig.java
@@ -0,0 +1,31 @@
+package truckmanagementproject.config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(new HashMap<String, Object>() {{
+            put("cloud_name", cloudName);
+            put("api_key", apiKey);
+            put("api_secret", apiSecret);
+        }});
+    }
+}
---
+++ src/main/java/truckmanagementproject/services/impl/CloudinaryServiceImpl.java
@@ -0,0 +1,31 @@
+package truckmanagementproject.services.impl;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+import truckmanagementproject.services.CloudinaryService;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class CloudinaryServiceImpl implements CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    @Autowired
+    public CloudinaryServiceImpl(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    @Override
+    public String upload(MultipartFile multipartFile) throws IOException {
+
+        File file = File.createTempFile("temp-file", multipartFile.getOriginalFilename());
+        multipartFile.transferTo(file);
+
+        return cloudinary.uploader().upload(file, new HashMap())
+                .get("url")
+                .toString();
+    }
+}
