8c9cf7f03f7750058181c5cef987918889581edb
alexurumov <alex.urumov@gmail.com>
2019-12-04 12:43:31 +0200

    Add Trip Get Mapping

--- src/main/java/truckmanagementproject/web/controllers/trips/TripController.java
+++ src/main/java/truckmanagementproject/web/controllers/trips/TripController.java
@@ -1,17 +1,51 @@
 package truckmanagementproject.web.controllers.trips;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import truckmanagementproject.services.DriverService;
+import truckmanagementproject.services.VehicleService;
+import truckmanagementproject.web.models.drivers.DriverViewModel;
 import truckmanagementproject.web.models.trips.AddTripModel;
+import truckmanagementproject.web.models.vehicles.VehicleViewModel;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class TripController {
 
+    private final DriverService driverService;
+    private final VehicleService vehicleService;
+    private final ModelMapper mapper;
+
+    @Autowired
+    public TripController(DriverService driverService, VehicleService vehicleService, ModelMapper mapper) {
+        this.driverService = driverService;
+        this.vehicleService = vehicleService;
+        this.mapper = mapper;
+    }
+
     @GetMapping("/trips/add")
-    public String getTripAddForm() {
-        return "trips/add-trip";
+    public ModelAndView getTripAddForm(ModelAndView modelAndView) {
+        List<DriverViewModel> drivers = driverService.getAllDrivers()
+                .stream()
+                .map(driver -> mapper.map(driver, DriverViewModel.class))
+                .collect(Collectors.toList());
+        modelAndView.addObject("drivers", drivers);
+
+        List<VehicleViewModel> vehicles = vehicleService.getAllVehicles()
+                .stream()
+                .map(vehicle -> mapper.map(vehicle, VehicleViewModel.class))
+                .collect(Collectors.toList());
+
+        modelAndView.addObject("vehicles", vehicles);
+        modelAndView.setViewName("trips/add");
+        return modelAndView;
     }
 
     @PostMapping("/trips/add")
