09c3f6d7ef506dda25f0e49300f9aa109f54818d
alexurumov <alex.urumov@gmail.com>
2019-12-16 14:22:33 +0200

    Summary Service + Controller + View Completed!

--- src/main/java/truckmanagementproject/web/controllers/summaries/SummaryController.java
+++ src/main/java/truckmanagementproject/web/controllers/summaries/SummaryController.java
@@ -1,23 +1,43 @@
 package truckmanagementproject.web.controllers.summaries;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import truckmanagementproject.services.services.auth.AuthService;
+import truckmanagementproject.services.services.summaries.SummaryService;
+import truckmanagementproject.web.models.auth.LoginUserViewModel;
+import truckmanagementproject.web.models.summaries.SummaryViewModel;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class SummaryController {
 
+    private final SummaryService summaryService;
+    private final AuthService authService;
+    private final ModelMapper mapper;
+
+    @Autowired
+    public SummaryController(SummaryService summaryService, AuthService authService, ModelMapper mapper) {
+        this.summaryService = summaryService;
+        this.authService = authService;
+        this.mapper = mapper;
+    }
+
     @GetMapping("/summary/overall")
-    public String getOverallSummary() {
-        return "summary/overall-summary";
-    }
+    public ModelAndView getOverallSummary(ModelAndView modelAndView, HttpSession session) throws Exception {
 
-    @GetMapping("/summary/by-truck")
-    public String getSummaryByTruck() {
-        return "summary/summary-by-truck";
-    }
+        LoginUserViewModel user = (LoginUserViewModel) session.getAttribute("user");
+        if (!authService.isUserManager(user) && !authService.isUserAdmin(user)) {
+            throw new Exception("Unauthorized user");
+        }
 
-    @GetMapping("/summary/by-driver")
-    public String getSummaryByDriver() {
-        return "summary/summary-by-driver";
+        SummaryViewModel summary = mapper.map(summaryService.getSummary(), SummaryViewModel.class);
+
+        modelAndView.addObject("summary", summary);
+        modelAndView.setViewName("summary/overall-summary");
+        return modelAndView;
     }
 }
